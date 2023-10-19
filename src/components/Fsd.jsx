22eb1fe8e1688bc4eacfeb89fcce28752402cd9e
partha2001ps <parthapn2017@gmail.com>
2023-10-19 18:53:37 +0530

    add

--- src/components/Fsd.jsx
+++ src/components/Fsd.jsx
@@ -2,12 +2,26 @@ import React from 'react'
 
 export default function Fsd() {
   return (
-      <div>
-          <div className='card' style={{width: "18rem"}}>
+      <div className='card-group'>
+          <div className='card d-inline-block m-4' style={{width: "18rem"}}>
               <img src="https://www.guvi.in/blog/wp-content/uploads/2023/08/Feature-image-Best-Full-Stack-Development-Online-Courses.webp" className='card-img-top' alt="" />
               <div className='card-body'>
                   <h5 className='card-title'>7 Best Full-Stack Development Online Courses [2023]</h5>
                   <p className='card-text'>Today’s world is rapidly evolving into a technological landscape. Owing to these dynamics, the demand</p>
+              </div>
+          </div>
+          <div className='card d-inline-block  m-4' style={{width: "18rem"}}>
+              <img src="https://www.guvi.in/blog/wp-content/uploads/2023/08/The-Ultimate-Guide-to-Real-World-Full-Stack-Development-Applications-2048x1072.png" className='card-img-top' alt="" />
+              <div className='card-body'>
+                  <h5 className='card-title'>The Ultimate Guide to Real-World Full-Stack Development Applications</h5>
+                  <p className='card-text'>Full-stack development has become increasingly popular in recent years, with companies seeking professionals who can</p>
+              </div>
+          </div>
+          <div className='card d-inline-block  m-4' style={{width: "18rem"}}>
+              <img src="https://www.guvi.in/blog/wp-content/uploads/2023/08/The-Top-10-Tools-Every-Full-Stack-Developer-Should-Master-in-2023-2048x1072.png" className='card-img-top' alt="" />
+              <div className='card-body'>
+                  <h5 className='card-title'>The Top 10 Tools Every Full-Stack Developer Should Master in 2023</h5>
+                  <p className='card-text'>‍As a full-stack developer, having the right set of tools is crucial for your success.</p>
               </div>
           </div>
     </div>
